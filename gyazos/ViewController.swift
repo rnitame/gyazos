fb1cecb170bd33662394e4d50c0c5a7b7551d40d
Ryo Nitami <nitry.n2t1m1@rnita.me>
2017-02-07 00:16:18 +0900

    initial commit

---
+++ gyazos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gyazos
+//
+//  Created by Ryo Nitami on 2017/02/06.
+//  Copyright © 2017 Ryo Nitami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
