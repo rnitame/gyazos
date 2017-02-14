9af5dce35b2c287d1b7add2f3ee044d0b6bca4d1
Ryo Nitami <nitry.n2t1m1@rnita.me>
2017-02-15 01:57:55 +0900

    add: storyboard settings

--- gyazos/ViewController.swift
+++ gyazos/ViewController.swift
@@ -2,24 +2,19 @@
 //  ViewController.swift
 //  gyazos
 //
-//  Created by Ryo Nitami on 2017/02/06.
+//  Created by Ryo Nitami on 2017/02/15.
 //  Copyright © 2017 Ryo Nitami. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad();
     }
-
+    
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        super.didReceiveMemoryWarning();
     }
-
-
 }
-
