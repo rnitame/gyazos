41e6c74239519a9c716d9bbbba7fde338ab9d774
Ryo Nitami <nitry.n2t1m1@rnita.me>
2017-02-15 04:01:04 +0900

    add: title and button on view

--- gyazos/ViewController.swift
+++ gyazos/ViewController.swift
@@ -11,10 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
-        super.viewDidLoad();
+        super.viewDidLoad()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning();
-    }
 }
