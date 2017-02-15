21fde3bb147853e08bd6772697f2e789fff290b7
Ryo Nitami <nitry.n2t1m1@rnita.me>
2017-02-16 01:51:56 +0900

    add: uploadImage method

--- gyazos/ViewController.swift
+++ gyazos/ViewController.swift
@@ -7,11 +7,36 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // uploadImage()
     }
     
+    func uploadImage() {
+        let url = try! URLRequest(
+            url: URL(string: "upload.gyazo.com/upload.cgi")!,
+            method: .post
+        )
+        let idData: Data! = "".data(using: .utf8)
+        Alamofire.upload(
+            multipartFormData: { multipartFormData in
+                multipartFormData.append(idData, withName: "id")
+            },
+            with: url,
+            encodingCompletion: { encodingResult in
+                switch encodingResult {
+                case .success(let upload, _, _):
+                    upload.responseJSON { response in
+                        debugPrint(response)
+                    }
+                case .failure(let encodingError):
+                    print(encodingError)
+                }
+            }
+        )
+    }
 }
