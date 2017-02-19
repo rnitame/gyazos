7dc03c193b11ded8aa456f936541b956546f02ab
Ryo Nitami <nitry.n2t1m1@rnita.me>
2017-02-20 01:03:54 +0900

    add: deploygate framework && cocoapods

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '9.0'
+
+target 'gyazos' do
+    pod 'DeployGateSDK'
+end
---
+++ Pods/Target Support Files/Pods-gyazos/Pods-gyazos-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gyazos : NSObject
+@end
+@implementation PodsDummy_Pods_gyazos
+@end
